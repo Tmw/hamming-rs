16b3bb9217be7ba7f7bab2940fd45f8e5434be26
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2020-10-01 22:31:20 +0200

    Add function to convert back to string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// TODO: 
+// - Break out the BitVec module
+// - Can we flip the order of most of the ranges (16..0)? and shift the data right and and with a
+// one instead?
+//
+// Add function that randomly flips a bit
+// Add a function that repairs errors
+// done.
 struct BitVec {
     source: Vec<u8>,
     index: usize
@@ -29,38 +37,99 @@ impl Iterator for BitVec {
 }
 
 fn main() {
-    let blocks = to_blocks("abc".as_bytes());
-    for block in blocks {
-        let parity = calculate_parity(&block);
-        let block = assign_parity(&block, &parity);
+    let blocks : Blocks = Blocks::from("abc");
+
+    let blocks = Blocks(blocks.0.iter().map(|block| {
+        let parity = calculate_parity(block);
+        assign_parity(block, &parity)
+    }).collect());
+
+    for block in &blocks.0 {
         print_block(block);
     }
+
+    let out = to_string(blocks);
+    println!("After encode and decode: {}", out);
 }
 
-fn to_blocks(data: &[u8]) -> Vec<u16> {
-    // allocate block buffer
-    let mut blocks = Vec::new();
+struct Blocks (Vec<u16>);
+impl From<&str> for Blocks {
+    fn from(data: &str) -> Blocks {
+        Blocks::from(data.as_bytes())
+    }
+}
 
-    // make the data ierable per byte
-    let mut bits = BitVec::from_bytes(data).peekable();
+impl From<&[u8]> for Blocks {
+    fn from(data: &[u8]) -> Blocks {
+        Blocks::new(data)
+    }
+}
 
-    // iterate until theres no more bits
-    while bits.peek().is_some() {
-        let block = (1..16)
-            .filter(|i|  !usize::is_power_of_two(*i))
-            .fold(0u16, |acc, i| {
-                match bits.next() {
-                    Some(true) =>  acc | (0b1 << 15 - i),
-                    Some(false) => acc | (0b0 << 15 - i),
-                    None => acc
-                }
-            });
+impl Blocks {
+    fn new(data: &[u8]) -> Self {
+        // allocate block buffer
+        let mut blocks = Vec::new();
 
-        blocks.push(block)
+        // make the data ierable per byte
+        let mut bits = BitVec::from_bytes(data).peekable();
+
+        // iterate until theres no more bits
+        while bits.peek().is_some() {
+            let block = (1..16)
+                .filter(|i|  !usize::is_power_of_two(*i))
+                .fold(0u16, |acc, i| {
+                    match bits.next() {
+                        Some(true) =>  acc | (0b1 << 15 - i),
+                        Some(false) => acc | (0b0 << 15 - i),
+                        None => acc
+                    }
+                });
+
+            blocks.push(block)
+        }
+
+        Blocks(blocks)
+    }
+}
+
+fn to_string(blocks: Blocks) -> String {
+    let mut bits:Vec<bool> = Vec::new();
+
+    // convert the blocks into a long stream of booleans
+    for block in blocks.0 {
+        for bit in 1..16 {
+
+            // don't read the parity bits as data
+            if usize::is_power_of_two(bit) {
+                continue;
+            }
+
+            bits.push(block & 0b1 << 15 - bit > 0)
+        }
     }
 
-    blocks
+    let mut bytes: Vec<u8> = Vec::new();
+
+    // then from that vector; chunks of 8 to get the bits
+    for chunk in bits.chunks(8) {
+        if chunk.len() < 8 {
+            continue;
+        }
+        let mut byte = 0u8;
+
+        for (idx, bit) in chunk.iter().enumerate() {
+            match bit {
+                true =>  byte |= 0b1 << 7- idx,
+                false => byte |= 0b0 << 7 - idx,
+            }
+        }
+
+        &bytes.push(byte);
+    }
+
+    String::from_utf8(bytes).unwrap_or_default()
 }
+
 
 fn calculate_parity(data: &u16) -> u8 {
     (0..16)
@@ -81,7 +150,7 @@ fn assign_parity(data: &u16, parity: &u8) -> u16 {
         })
 }
 
-fn print_block(block: u16) {
+fn print_block(block: &u16) {
     for bit in 0..16 {
 
         // for every four bits, start on new line
