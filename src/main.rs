db75f9dff23006ff84bdca0173bac22454e775a2
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2020-10-13 23:07:56 +0200

    Read from stdin

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ mod blocks;
 use blocks::Blocks;
 
 fn main() {
+    let input = get_input();
+
     // initial loading data in Hamming code and assign parity
-    let mut blocks : Blocks = Blocks::from("a bit longer string");
+    let mut blocks : Blocks = Blocks::from(&input[..]);
     println!("Before noise:\t {}", &blocks.to_string());
 
     // introduce noise randomly to the blocks and see its output
@@ -16,5 +18,11 @@ fn main() {
     // then repair using the parity
     blocks.repair();
     println!("Repaired:\t {}", &blocks.to_string());
+}
+
+fn get_input() -> String {
+    std::env::args()
+        .nth(1)
+        .unwrap_or(String::from("This is a sample string"))
 }
 
