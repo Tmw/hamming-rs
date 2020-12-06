462b521714e8397a378c34492b2a3d3c8a2b7f9d
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2020-12-06 14:31:32 +0100

    Fix encoding/decoding bug

--- src/lib/blocks.rs
+++ src/lib/blocks.rs
@@ -17,22 +17,17 @@ impl Blocks {
     /// `new` will take bytes and convert them to 16 bit blocks where 11 bits
     /// are used to represent data and 5 bits are reseved for parity bits.
     ///
-    /// pass is_raw to indicate the input data already contains parity bits.
-    /// the `new` function will then skipp leaving space for them
+    /// Pass is_raw to indicate the input data already contains parity bits,
+    /// the 16 bits will then be assigned per block as is.
     pub fn new(data: &[u8], is_raw: bool) -> Self {
-        // allocate block buffer
         let mut blocks = Vec::new();
-
-        // make the data iterable per byte
-        let bits = BitVec::from_bytes(data);
-
-        let mut bits = bits.peekable();
+        let mut bits = BitVec::from_bytes(data).peekable();
 
         // iterate until theres no more bits
         while bits.peek().is_some() {
-            let block = (0..15usize)
+            let block = (0..16u8)
                 .rev()
-                .filter(|bit| is_raw || !usize::is_power_of_two(15 - bit))
+                .filter(|bit| is_raw || !Blocks::is_parity_bit(15 - bit))
                 .fold(0u16, |block, bit| match bits.next() {
                     Some(true) => block | 0b1 << bit,
                     Some(false) => block | 0b0 << bit,
@@ -43,6 +38,11 @@ impl Blocks {
         }
 
         Blocks(blocks)
+    }
+
+    // reserve 0th bit for parity; even though it wont be utilized for now.
+    fn is_parity_bit(index: u8) -> bool {
+        index == 0 || index.is_power_of_two()
     }
 
     pub fn iter(&self) -> std::slice::Iter<'_, Block> {
@@ -68,8 +68,8 @@ impl Blocks {
     fn serialize(&self, strip_from_parity: bool) -> Vec<bool> {
         self.iter()
             .flat_map(|block| {
-                (1..16usize)
-                    .filter(|bit| !(strip_from_parity && bit.is_power_of_two()))
+                (0..16_u8)
+                    .filter(|bit| !(strip_from_parity && Blocks::is_parity_bit(*bit)))
                     .map(move |bit| block.data >> (15 - bit) & 0b1 > 0)
             })
             .collect()
@@ -93,7 +93,6 @@ impl Blocks {
 
     /// convert data back to readable string stripping each block from its parity
     pub fn to_string(&self) -> String {
-        // chunk per 8 bits to turn into bytes
         let chars: Vec<char> = self
             .serialize(true)
             .chunks(8)
@@ -123,29 +122,53 @@ mod blocks_test {
     use super::Blocks;
     #[test]
     fn new_passed_bytes_returns_blocks_leaving_space_for_parity() {
-        let data = "some bytes";
-        let blocks = Blocks::new(data.as_bytes(), false);
+        let data = [0xff, 0xff, 0xff, 0xff];
+        let blocks = Blocks::new(&data, false);
 
-        // eleven data bits per block should yield 8 blocks for 10 bytes
-        let expected_num_blocks = (data.len() as f64 * 8.0 / 11.0).ceil() as usize;
-
-        // expect 8 blocks
-        assert_eq!(blocks.iter().as_slice().len(), expected_num_blocks);
+        assert_eq!(blocks.iter().as_slice().len(), 3);
     }
 
     #[test]
     fn new_passed_bytes_and_raw_leaves_bits_untouched() {
-        let data = "some bytes".as_bytes();
+        let data = [0xff, 0xff, 0xff, 0xff];
         let blocks = Blocks::new(&data, true);
 
-        // ten bytes original, should return 5 blocks
-        assert_eq!(blocks.iter().as_slice().len(), 5);
+        assert_eq!(blocks.iter().as_slice().len(), 2);
     }
 
     #[test]
-    fn into_string_returns_stringified_version_of_original_input() {
-        let data = "blah";
+    fn to_string_returns_stringified_version_of_original_input() {
+        let data = "some bytes";
         let blocks = Blocks::new(data.as_bytes(), false);
+
         assert_eq!(blocks.to_string(), data);
+    }
+
+    #[test]
+    fn to_byte_vec_returns_raw_vec_of_u8() {
+        let data = [0xff, 0xff];
+        let blocks = Blocks::new(&data, true);
+
+        assert_eq!(blocks.to_byte_vec(), vec![0xff, 0xff])
+    }
+
+    #[test]
+    fn to_byte_vec_returns_prepared_vec_of_u8() {
+        let data = [0xff, 0xff];
+        let blocks = Blocks::new(&data, false);
+
+        assert_eq!(blocks.to_byte_vec(), vec![0x7f, 0xff, 0x3f, 0xc0])
+    }
+
+    #[test]
+    fn back_to_back() {
+        // encode
+        let data = "some bytes";
+        let blocks = Blocks::new(data.as_bytes(), false);
+        let raw_bytes = blocks.to_byte_vec();
+
+        // decode
+        let decode_blocks = Blocks::new(raw_bytes.as_slice(), true);
+        assert_eq!(decode_blocks.to_string(), data);
     }
 }
